44910cc781bd03ebe9761864352d83be27f1b027
Harsh kumar <mister.harshkumar@gmail.com>
2024-09-02 23:56:46 +0530

    refactor: Update usermodel.js to change default role from 'teacher' to 'admin'

--- Backend/models/usermodel.js
+++ Backend/models/usermodel.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
 
-  role:{type:String,required:true,enum:['manager','teacher'],default:'teacher'},
+  role:{type:String,required:true,enum:['admin','teacher'],default:'teacher'},
   reports: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Class' }],
 });
 
