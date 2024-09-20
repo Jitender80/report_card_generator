d6c38f3a4ddacb21afea6e0ad9f82ff6b0ae1f61
Harsh kumar <mister.harshkumar@gmail.com>
2024-09-19 23:32:26 -0700

    Refactor render-build.sh to store/pull Puppeteer cache with build cache

--- Backend/render-build.sh
+++ Backend/render-build.sh
@@ -1,16 +1,14 @@
 #!/usr/bin/env bash
-# exit on error
-set -o errexit
+# exit on errorset -o errexit
 
-# Install system dependencies
-apt-get update
-apt-get install -y wget gnupg
-
-# Install Chrome
-wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
-sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
-apt-get update
-apt-get install -y google-chrome-stable
-
-# Install Node.js dependencies
 npm install
+# npm run build # uncomment if required
+
+# Store/pull Puppeteer cache with build cache
+if [[ ! -d $PUPPETEER_CACHE_DIR ]]; then 
+  echo "...Copying Puppeteer Cache from Build Cache" 
+  cp -R $XDG_CACHE_HOME/puppeteer/ $PUPPETEER_CACHE_DIR
+else 
+  echo "...Storing Puppeteer Cache in Build Cache" 
+  cp -R $PUPPETEER_CACHE_DIR $XDG_CACHE_HOME
+fi
